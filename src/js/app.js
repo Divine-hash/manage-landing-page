e6196a9de00d1e7077476fa3edc97ba9f2e2b0c6
Divine <ifeanyidivineonuoha@gmail.com>
2025-03-31 09:57:50 +0100

    implement testimonial section swiper

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
 import '../css/styles.css';
-
 import {initializeMenu} from './modules/menu';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
 
 const menuBtn = document.querySelector('[data-id="menu-btn"]');
 initializeMenu(menuBtn);
 
+new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  breakpoints: {
+    800: {
+      slidesPerView: 2.5,
+    }
+  }
+});
